b6a9334d552c8e9ef12de966ee14ebc9f06ef1ac
sonselin <sonse@Sonses-MacBook-Pro.local>
2024-09-27 14:00:46 +0300

    tests for database add/delete/get user

--- tests/database_test.go
+++ tests/database_test.go
@@ -25,20 +25,11 @@ func TestDBConnection(t *testing.T) {
 }
 
 func TestDBGetUserById(t *testing.T) {
-	connStr := "user=postgres dbname=medods password=postgres host=localhost sslmode=disable"
-	db, err := sql.Open("postgres", connStr)
+	db, err := logic.GetDBConn()
 	if err != nil {
-		t.Fatalf("Failed to open the database: %v", err)
+		t.Fatal("error connected to db")
 	}
 	defer db.Close()
-
-	// Check if the connection is valid
-	if err := db.Ping(); err != nil {
-		t.Fatalf("Failed to ping the database: %v", err)
-	}
-
-	t.Log("Successfully connected to the database!")
-
 	user, err := logic.GetUserById(2, db)
 	if err != nil {
 		t.Fatalf("Failed to get user by id: %s", err)
@@ -46,4 +37,30 @@ func TestDBGetUserById(t *testing.T) {
 	if user.Name == "maxim" {
 		t.Log("Test passed!")
 	}
+}
+
+func TestDBAddUserIntoTable(t *testing.T) {
+	db, err := logic.GetDBConn()
+	if err != nil {
+		t.Fatal("error connected to db")
+	}
+	defer db.Close()
+	err = logic.AddUserIntoTable(db, "pashka", "228.228.227.012", "pashka@mail.ru")
+	if err != nil {
+		t.Fatalf("Failed to add user: %s", err)
+	}
+	t.Log("Test passed!")
+}
+
+func TestDBDeleteUserFromTable(t *testing.T) {
+	db, err := logic.GetDBConn()
+	if err != nil {
+		t.Fatal("error connected to db")
+	}
+	defer db.Close()
+	err = logic.DeleteUserFromTable(db, "name", "pashka")
+	if err != nil {
+		t.Fatalf("Failed to delete user from table: %s", err)
+	}
+	t.Log("Test passed!")
 }
